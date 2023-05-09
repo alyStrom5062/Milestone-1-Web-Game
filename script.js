9828ae0fe998a94a8ea7a1546ada66a3cf973e33
alyStrom5062 <Alyenastrom00@gmail.com>
2023-05-08 20:08:34 -0700

    Added interactable puzzle Objects

--- script.js
+++ script.js
@@ -2,19 +2,16 @@
 // ==============================================================================================
 // ==============================================================================================
 // STEP 1
-// determine which map img is being used and what bounds it has.
+// create map array + assign player tile + grab PC sprite + 
 
 //grab Tema's sprite id in the HTML doc
 let pcImage = document.getElementById("playerCharacter")
 
-let currentMap = "map1"
-
     // map is 17x13
+        //draws map. default allows all tiles to be walked on.
+        // each tile is 64x64px
 
-    //draws map. default allows all tiles to be walked on.
-    // each tile is 64x64px
-
-//            1(-1088) 2(-1024) 3(-960) 4(-896) 5(-832) 6(-768) 7(-704) 8(-640) 9(-576) 10(-512) 11(-448) 12(-384) 13(-320) 14(-256) 15(-192) 16(-128) 17(-64)
+//    1(-1088) 2(-1024) 3(-960) 4(-896) 5(-832) 6(-768) 7(-704) 8(-640) 9(-576) 10(-512) 11(-448) 12(-384) 13(-320) 14(-256) 15(-192) 16(-128) 17(-64)
 
         //              1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17
         let mapTiles = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,  //1     (-768)
@@ -32,7 +29,11 @@ let currentMap = "map1"
                         1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,  //13    (0)
                     ]
         
-    let playerTile = 71    // tile: 3x5: 17*4 + 3 = 71 (-512, -960)
+    let playerTile = 71    // tile: 3x5: 17*4 + 3 = 71 (-960, -512)
+
+    let playerCoordx =  -960
+    let playerCoordy = -512
+
     console.log(playerTile)
 
 // ==============================================================================================
@@ -49,24 +50,28 @@ let currentMap = "map1"
         if(e.key === "ArrowLeft"){
             direction = "Left"
             pcImage.src = "assets/pcTemaLeft.png"
+            walk()
         }
         if(e.key === "ArrowUp"){
             direction = "Up"
             pcImage.src = "assets/pcTemaUp.png"
+            walk()
 
         }
         if(e.key === 'ArrowRight'){
             direction = "Right"
             pcImage.src = "assets/pcTemaRight.png"
+            walk()
 
         }
         if(e.key === 'ArrowDown'){
             direction = "Down"
             pcImage.src = "assets/pcTemaDown.png"
-
+            walk()
         }
-        walk()
-        console.log(direction)
+        if(e.key === "Enter"){
+            interact()
+        }
     })
 
 // ==============================================================================================
@@ -75,14 +80,9 @@ let currentMap = "map1"
 // STEP 3
 // move character to new tile onpress according to his direction
 
-        // limit on where you can walk based on the number of the Tile.
-
-let playerCoordx =  -960
-let playerCoordy = -512
-
 function walk(){ 
+    collision()
     if(direction === "Left"){
-        playerTile -= 1
         playerCoordx -= 64
         if(playerCoordx < -960){
             playerCoordx = -960
@@ -122,6 +122,7 @@ function walk(){
         }
 
     }
+
     console.log(playerTile)
     console.log(playerCoordx + ", " + playerCoordy)
 }
@@ -130,8 +131,123 @@ function walk(){
 // ==============================================================================================
 // ==============================================================================================
 // STEP 4
-// move character to new tile onpress according to his direction
+// function to create each npc and object on map.
+
+function newObject(src, id, top, left){
+    let image = document.createElement('img')
+    image.src = src
+    image.setAttribute("id", id)
+    image.style.position = "relative"
+    image.style.top = top + "px"
+    image.style.left = left + "px"
+    document.body.append(image)
+    return image
+}
+
+newObject("assets/npcRubinDown.png", "rubinNPC", -720, 704)
+
+newObject("assets/pillarRed.png", "pillarRed", -720, 128)
+newObject("assets/pillarBlue.png", "pillarBlue", -720, 256)
+newObject("assets/pillarPurple.png", "pillarPurple", -720, 448)
+newObject("assets/pillarWhite.png", "pillarWhite", -720, 576)
+
+newObject("assets/buttons/buttonUpRed.png", "redPlate", -438, 0)
+newObject("assets/buttons/buttonUpBlue.png", "bluePlate", -438, 64)
+newObject("assets/buttons/buttonUpPurple.png", "purplePlate", -438, 128)
+newObject("assets/buttons/buttonUpWhite.png", "whitePlate", -438, 192)
+
+newObject("assets/buttons/leverUpRed.png", "redLever", -784, -320)
+newObject("assets/buttons/leverUpPurple.png", "purpleLever", -784, -128)
+newObject("assets/buttons/leverUpWhite.png", "whiteLever", -784, 64)
+
+// ==============================================================================================
+// ==============================================================================================
+// ==============================================================================================
+// STEP 5
+// Other Objects and NPCs are impassable based on tile number.
+
+let impassableObjects = ["rubinNPC", "pillarRed", "pillarBlue", "pillarPurple", "pillarWhite"]
+
+function collision(){
+    if(direction === "Up" && playerTile === 89){
+        playerCoordy = -448
+    }
+}
 
 
+// ==============================================================================================
+// ==============================================================================================
+// ==============================================================================================
+// STEP 6
+// Objects are interactible.
+
+        let redLever = document.getElementById("redLever")
+        let purpleLever = document.getElementById("purpleLever")
+        let whiteLever = document.getElementById("whiteLever")
+
+        let redPlate = document.getElementById("redPlate")
+        let bluePlate = document.getElementById("bluePlate")
+        let purplePlate = document.getElementById("purplePlate")
+        let whitePlate = document.getElementById("whitePlate")
+
+        function interact(){
+
+            // Levers
+
+                if(direction === "Up" && playerTile === 73){
+                    redLever.src = "assets/buttons/leverDownRed.png"
+                    console.log("Red Lever Pulled")
+                }
+                if(direction === "Up" && playerTile === 77){
+                    purpleLever.src = "assets/buttons/leverDownPurple.png"
+                    console.log("Purple Lever Pulled")
+                }
+                if(direction === "Up" && playerTile === 81){
+                    whiteLever.src = "assets/buttons/leverDownWhite.png"
+                    console.log("White Lever Pulled")
+                }
 
 
+            // Plate Buttons
+
+            if(playerTile === 142){
+                redPlate.src = "assets/buttons/buttonDownRed.png"
+                console.log("Red Plate Pressed")
+            }
+            if(playerTile === 144){
+                bluePlate.src = "assets/buttons/buttonDownBlue.png"
+                console.log("Blue Plate Pressed")
+            }
+            if(playerTile === 146){
+                purplePlate.src = "assets/buttons/buttonDownPurple.png"
+                console.log("Purple Plate Pressed")
+            }
+            if(playerTile === 148){
+                whitePlate.src = "assets/buttons/buttonDownWhite.png"
+                console.log("White Plate Pressed")
+            }
+        }
+
+// ==============================================================================================
+// ==============================================================================================
+// ==============================================================================================
+// STEP 7
+// Rubin Interact.
+
+// ==============================================================================================
+// ==============================================================================================
+// ==============================================================================================
+// STEP 8
+// Plate Puzzle
+
+// ==============================================================================================
+// ==============================================================================================
+// ==============================================================================================
+// STEP 9
+// Lever Puzzle
+
+// ==============================================================================================
+// ==============================================================================================
+// ==============================================================================================
+// STEP 10
+// The End
